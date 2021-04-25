cb3b5e1b67f89b8905600a94213e930f863fbb90
Ali Agboola <aliagboola1@gmail.com>
2021-04-25 12:54:36 +0100

    link of file

--- src/blogapp/admin.py
+++ src/blogapp/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostDB(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'author',
+    ]
+    
+admin.site.register(Post, PostDB)
