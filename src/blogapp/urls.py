a364625e5703fd4c8308b2935bfe62ab5eeca231
Ali Agboola <aliagboola1@gmail.com>
2021-04-25 11:51:21 +0100

    make a sample view

--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', view, name),
+    path('', views.sample_view, name = 'sample'),
 ]
--- src/blogapp/views.py
+++ src/blogapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def sample_view(request):
+    return HttpResponse("<h1>i made a view</h1>")
