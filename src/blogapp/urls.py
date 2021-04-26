1e5d5d7a8f5473a120f89fe45d0fb256080ee26b
Ali Agboola <aliagboola1@gmail.com>
2021-04-26 18:25:16 +0100

    correct urls.py

--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
+    path('post/<int:pk>', BlogDetailView.as_view(), name = 'post_details'),
     path('', BlogListView.as_view(), name = 'blogapp-home'),
-    path('post_detail/<int:pk>', BlogDetailView.as_view(), name = 'post-details'),
 ]
