2acf76ed8489740ec8aea4503dad74b01dc1442b
Ali Agboola <aliagboola1@gmail.com>
2021-04-26 18:33:59 +0100

    correction to views and post page

--- src/blogapp/views.py
+++ src/blogapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.views.generic.list import ListView, DetailView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 # Create your views here.
