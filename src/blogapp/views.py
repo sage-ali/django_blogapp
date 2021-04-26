2de5a458d293ef4f204577dcbe55d12449c9867b
Ali Agboola <aliagboola1@gmail.com>
2021-04-26 18:08:52 +0100

    create detail view and add route to urls

--- src/blogapp/views.py
+++ src/blogapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.views.generic.list import ListView
+from django.views.generic.list import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
