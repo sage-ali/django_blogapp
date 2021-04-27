b829cb92fa006defe659dd41d7133c77d8550e3e
Ali Agboola <aliagboola1@gmail.com>
2021-04-27 06:06:23 +0100

    correct and complete update view

--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
-    path('post/<int:pk/edit>', BlogUpdateView.as_view(), name = 'edit_post'),
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name = 'edit_post'),
     path('post/new/', BlogCreateView.as_view(), name = 'new_post'),
     path('post/<int:pk>', BlogDetailView.as_view(), name = 'post_details'),
     path('', BlogListView.as_view(), name = 'blogapp-home'),
