96c90079e0d88757a47e92d0362f5156b3a0eb83
Ali Agboola <aliagboola1@gmail.com>
2021-04-29 16:23:09 +0100

    add route for password reset. auth_views used

--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
@@ -14,4 +16,24 @@ urlpatterns = [
     path('post/<int:pk>', views.BlogDetailView.as_view(), name = 'post_details'),
     
     path('', views.BlogListView.as_view(), name = 'blogapp-home'),
+    
+    
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    
+    # path('reset_password/',
+    #     auth_views.PasswordResetView.as_view(template_name="password/password_reset.html"),
+    #     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="password/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_form.html"), 
+        name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), 
+        name="password_reset_complete"),
+
 ]
