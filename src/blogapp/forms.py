8d0c9cffa17b018e75e46916816ef1d071b8c5c7
Ali Agboola <aliagboola1@gmail.com>
2021-04-29 19:35:50 +0100

    model form for comment model

--- src/blogapp/forms.py
+++ src/blogapp/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
+from django.forms import ModelForm
+from .models import *
 
 
 
@@ -8,3 +9,8 @@ class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+class CommentForm(ModelForm):
+	class Meta:
+		model = Comment
+		fields = '__all__'
