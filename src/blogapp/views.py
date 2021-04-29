94a1ea93c7765e1471a7ee8145b9d6b13b56b1c0
Ali Agboola <aliagboola1@gmail.com>
2021-04-29 16:24:00 +0100

    add view for rest password

--- src/blogapp/views.py
+++ src/blogapp/views.py
@@ -15,6 +15,50 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .forms import CreateUserForm
 
+
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+
+def password_reset_request(request):
+	if request.method == "POST":
+		
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			
+			data = password_reset_form.cleaned_data['email']
+			associated_users = User.objects.filter(Q(email=data))
+			
+			if associated_users.exists():
+				
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "password/password_reset.txt"
+					c = {
+						"email":user.email,
+						'domain':'127.0.0.1:8000',
+						'site_name': 'Website',
+						"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+						"user": user,
+						'token': default_token_generator.make_token(user),
+						'protocol': 'http',
+					}
+    
+					email = render_to_string(email_template_name, c)
+					
+					try:
+						return redirect(email)
+					except:
+						return HttpResponse('Try again')
+					# return redirect ("reset_password_complete/")
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="password/password_reset.html", context={"password_reset_form":password_reset_form})
+
 # Create your views here.
 
 
