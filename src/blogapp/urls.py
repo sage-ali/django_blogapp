8551a1afb84db27272563736ca750fdbf23f061a
Ali Agboola <aliagboola1@gmail.com>
2021-04-29 19:34:24 +0100

    change details to function based view

--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('post/<int:pk>/delete', login_required(views.BlogDeleteView.as_view()), name = 'delete_post'),
     path('post/<int:pk>/edit', login_required(views.BlogUpdateView.as_view()), name = 'edit_post'),
     path('post/new/', login_required(views.BlogCreateView.as_view()), name = 'new_post'),
-    path('post/<int:pk>', views.BlogDetailView.as_view(), name = 'post_details'),
+    
+    
+    path('post/<int:pk>', views.blog_details, name = 'post_details'),
     
     path('', views.BlogListView.as_view(), name = 'blogapp-home'),
     
